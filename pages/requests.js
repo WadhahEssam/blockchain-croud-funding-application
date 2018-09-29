5e45395df2329f09a3dab32f000c9ed1940020fa
Wadah Esam <wadah-wleed@hotmail.com>
2018-09-29 15:23:10 +0300

    create new request page

--- pages/requests.js
+++ pages/requests.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import Router from 'next/router';
+import web3 from '../ethereum/web3';
+import compiledCampaign from '../ethereum/build/Campaign.json';
+import { Button } from 'semantic-ui-react';
 
 class Requests extends Component {
 
@@ -8,14 +11,20 @@ class Requests extends Component {
     campaignAddress: null,
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.setState({ campaignAddress: Router.router.query.campaignAddress });
+    const campaignContract = await new web3.eth.Contract(JSON.parse(compiledCampaign.interface), this.state.campaignAddress);
+
+
   }
+
+  visitCreateRequestPage = () => { Router.push({ pathname: '/requests/new', query: { campaignAddress: this.state.campaignAddress } })};
 
   render() {
     return(
       <Layout>
         <div>{this.state.campaignAddress}</div>
+        <Button primary onClick={this.visitCreateRequestPage}>Create Request</Button>
       </Layout>
     );
   }
---
+++ pages/requests/new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../../components/Layout';
+
+class NewRequest extends Component {
+  render() {
+    return(
+      <Layout>
+        <div>new request</div>
+      </Layout>
+    );
+  }
+}
+
+export default NewRequest;
